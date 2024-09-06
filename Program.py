004076905160395d0ffb8d30c87d35637eeb2f09
Avebave <arvid.hedstrom@elev.ga.ntig.se>
2024-09-06 09:34:05 +0200

    Första

---
+++ Program.py
@@ -0,0 +1,16 @@
+import random
+print ("Datorns första slag")
+datorSlag1 = random.randint(1,6)
+datorSlag2 = + random.randint(1,6)
+blank = str (input(""))
+print (datorSlag1)
+print ("Datorns andra slag")
+blank = str (input(""))
+print (datorSlag2)
+datorLiv = datorSlag2 + datorSlag1
+print ("Datorns totala liv: " +str(datorLiv))
+blank = str(input(""))
+print("Ditt första slag")
+spelarSlag1 = random.randint(1,6)
+blank = str(input(""))
+print(spelarSlag1)
