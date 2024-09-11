40f4cef840e05a04a29f8f281ae9206146da0197
Avebave <arvid.hedstrom@elev.ga.ntig.se>
2024-09-11 13:36:52 +0200

    9/11

--- Program.py
+++ Program.py
@@ -2,6 +2,7 @@ import random
 print ("Datorns första slag")
 datorSlag1 = random.randint(1,6)
 datorSlag2 = + random.randint(1,6)
+allaLever = True
 blank = str (input(""))
 print (datorSlag1)
 print ("Datorns andra slag")
@@ -14,3 +15,71 @@ print("Ditt första slag")
 spelarSlag1 = random.randint(1,6)
 blank = str(input(""))
 print(spelarSlag1)
+print("Ditt andra slag")
+spelarSlag2 = random.randint(1,6)
+blank = str(input(""))
+print(spelarSlag2)
+spelarLiv = spelarSlag1 + spelarSlag2
+blank = str(input(""))
+print("Ditt totala liv: " + str(spelarLiv))
+blank = str(input(""))
+while allaLever:
+    print ("Datorns slag")
+    datorSlag1 = random.randint(1,6)
+    blank = str(input(""))
+    print("Datorn slog " + str(datorSlag1))
+    blank = str(input(""))
+    if datorSlag1 > 3:
+        print("Datorn gör " + str(datorSlag1) + " skada på dig")
+        spelarLiv = spelarLiv - datorSlag1
+        blank = str(input(""))
+        print("Du har " + str(spelarLiv) + " liv kvar")
+    else:
+        print (f'Datorn gör {datorSlag1} skada på sig själv')
+        datorLiv = datorLiv - datorSlag1
+        blank = str(input(""))
+        print (f'Datorn har {datorLiv} liv kvar')
+    if datorLiv <= 0:
+        blank = str(input(""))
+        print("Datorn dog lol")
+        allaLever = False
+        break
+    if spelarLiv <= 0:
+        blank = str(input(""))
+        print("Du dog lol")
+        allaLever = False
+        break
+    blank = str(input(""))
+    print("Ditt slag")
+    spelarSlag1 = random.randint(1,6)
+    blank = str(input(""))
+    print(f'Du slog {spelarSlag1}')
+    blank = str(input(""))
+    if spelarSlag1 > 3:
+        print(f'Du gör {spelarSlag1} skada på datorn')
+        datorLiv = datorLiv - spelarSlag1
+        blank = str(input(""))
+        print(f'Datorn har {datorLiv} liv kvar')
+    else:
+        print(f'Du gör {spelarSlag1} skada på dig själv')
+        spelarLiv = spelarLiv - spelarSlag1
+        blank = str(input(""))
+        print(f'Du har {spelarLiv} liv kvar')
+    if datorLiv <= 0:
+        blank = str(input(""))
+        print("Datorn dog lol")
+        allaLever = False
+        break
+    if spelarLiv <= 0:
+        blank = str(input(""))
+        print("Du dog lol")
+        allaLever = False
+        break
+    blank = str(input(""))
+blank = str(input(""))
+vinnare = ""
+if spelarLiv > datorLiv:
+    vinnare = "spelare"
+else:
+    vinnare = "datorn"
+print(f'{vinnare} vinner')
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Hervid Sprrong
+Grupp: Shervid Sprrang
+
+Målet är att man ska ha så mycket pengar efter 5 rundor (WIP)
+Först slås båda tärningar för att se hur mycket liv alla börjar med¨
+Efter det kan man betta så mycket man vill (WIP)
+Sen så tar man tur att slå med datorn som startar
+Om man slår 3 eller mindre så tar man själv skada
+Om man slår 4 eller mer så gör man skada på motståndarn
+Vinnarn är den som sist blir av med alla liv
